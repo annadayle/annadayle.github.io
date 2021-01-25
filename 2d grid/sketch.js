afa8171464d4a1cf0061acd841f2f8ae45ee2b56
annadayle <77638079+annadayle@users.noreply.github.com>
2021-01-25 13:41:21 -0600

    Revert "Update sketch.js"
    This reverts commit 4dd14519c6f2d0df144b161da4a82d10f83cb149.

--- 2d grid/sketch.js
+++ 2d grid/sketch.js
@@ -6,23 +6,15 @@ let grid = [[0,1,0,1,0],
             [0,1,0,1,0],
             [0,0,1,0,0]];
 
-let rows;
-let cols;
+let rows = grid.length;
+let cols = grid[0].length;
 let cellWidth;
 let cellHeight;            
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
-  // replace hardcoded grid with empty grid
-  grid = createEmptyGrid(30, 20);
-  
-  
-  rows = grid.length;
-  cols = grid[0].length;
   cellWidth = width / cols;
   cellHeight = height / rows;
-
 }
 
 function draw() {
@@ -41,27 +33,4 @@ function draw() {
 
     }
   }
-}
-
-function mousePressed() {
-  let x = Math.floor(mouseX / cellWidth);
-  let y = Math.floor(mouseY / cellHeight);
-
-  if (grid[y][x] === 1) {
-    grid[y][x] = 0;
-  }
-  else if (grid[y][x] === 0) {
-    grid[y][x] = 1;
-  }
-}
-
-function createEmptyGrid(cols, rows) {
-  let emptyGrid = [];
-  for (let y=0; y<rows; y++) {
-    emptyGrid.push([]);
-    for (let x=0; x<cols; x++) {
-      emptyGrid[y].push(0);
-    }
-  }
-  return emptyGrid;
 }
