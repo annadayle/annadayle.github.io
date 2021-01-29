c304c170f19b9e7dc754a86d3064182b41f841aa
annadayle <77638079+annadayle@users.noreply.github.com>
2021-01-28 21:45:52 -0600

    tic tacky toe

--- tic tac toe/sketch.js
+++ tic tac toe/sketch.js
@@ -3,24 +3,24 @@
 // You will be playing against the computer in this tic tac toe game. The computer will try to guess your moves and counter them, whether
 // that be making the game a tie or winning by making it's own row. Complete as many rounds as you want
 
-let grid = createEmptyGrid(3, 3);
-let rows, cols, cellWidth, cellHeight;
+const ROWS = 3
+const COLS = 3
+let grid, cellWidth, cellHeight;
 let s = 0;
-let t = "Your score: "
+let t = "Your score: ";
 
 function setup() {
   let myCanvas = createCanvas(windowWidth*0.8, windowHeight*0.8);
   myCanvas.position(windowWidth*0.1, windowHeight*0.1);
   
-  rows = grid.length;
-  cols = grid[0].length;
-  cellWidth = width*0.8 / cols;
-  cellHeight = height*0.8 / rows;
+  grid = createEmptyGrid(COLS, ROWS);
+  cellWidth = width*0.8 / COLS;
+  cellHeight = height*0.8 / ROWS;
 
 }
 
 function draw() {
-  background(220);
+  background(200);
   displayGrid();
   displayText();
 }
@@ -34,7 +34,7 @@ function mousePressed() {
 
 function toggleCell(x, y) {
   // check coordinates are in the array
-  if (x >= 0 && x < cols && y >=0 && y < rows) {
+  if (x >= 0 && x < COLS && y >=0 && y < ROWS) {
     if (grid[y][x] === 1) {
     grid[y][x] = 0;
   }
@@ -45,29 +45,31 @@ function toggleCell(x, y) {
 }
 
 function displayGrid() {
-    for (let y=0; y<rows; y++) {
-      for (let x=0; x<cols; x++) {
+    for (let y=0; y<ROWS; y++) {
+      for (let x=0; x<COLS; x++) {
         if (grid[y][x] === 0) {
           fill("white");
         }
         if (grid[y][x] === 1) {
-          fill("black");
+          fill("red");
         }
         rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
       }
     }
 }
 
+
 function displayText() {
   fill(50);
-  text (t + s, windowWidth -= windowWidth*0.1, windowHeight*0.1);
+  textAlign(LEFT, BOTTOM);
+  text (t + s, 50, 25);
 }
 
-function createEmptyGrid(cols, rows) {
+function createEmptyGrid(COLS, ROWS) {
   let empty = [];
-  for (let y=0; y<rows; y++) {
+  for (let y=0; y<ROWS; y++) {
     empty.push([]);
-    for (let x=0; x<cols; x++) {
+    for (let x=0; x<COLS; x++) {
       empty[y].push(0);
     }
   }
